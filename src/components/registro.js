5c07c6c7e237c1eee8cb0a89df2b52a1f46b71c6
oolidu <arquitecturaod@gmail.com>
2023-08-03 23:26:39 -0400

    Pequeña mejora al css

--- src/components/registro.js
+++ src/components/registro.js
@@ -1,27 +1,27 @@
 function registro(navigateTo) {
-    const section = document.createElement('section'); /*section se usa en registro y en login*/
-    const buttonReturn = document.createElement('button');
-    const inputName= document.createElement('input');
-    const inputEmail = document.createElement('input');
-    const inputPass = document.createElement('input');
-    const buttonRegistro = document.createElement('button');
-  
-    inputName.placeholder = 'Nombre de usuario';
-    inputEmail.placeholder = 'Correo';
-    inputPass.placeholder = 'Contraseña';
-  
-    buttonRegistro.textContent = 'registro';
+  const section = document.createElement('section'); /* section se usa en registro y en login */
+  const buttonReturn = document.createElement('button');
+  const inputName = document.createElement('input');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  const buttonRegistro = document.createElement('button');
 
-    buttonRegistro.addEventListener('click', () => {
-        navigateTo('/principal');
-    });
-  
-    buttonReturn.textContent = 'back to home';
-    buttonReturn.addEventListener('click', () => {
-      navigateTo('/');
-    });
-    section.append(inputName, inputEmail, inputPass, buttonRegistro, buttonReturn);
-    return section;
-  }
-  
-  export default registro;
+  inputName.placeholder = 'Nombre de usuario';
+  inputEmail.placeholder = 'Correo';
+  inputPass.placeholder = 'Contraseña';
+
+  buttonRegistro.textContent = 'registro';
+
+  buttonRegistro.addEventListener('click', () => {
+    navigateTo('/principal');
+  });
+
+  buttonReturn.textContent = 'back to home';
+  buttonReturn.addEventListener('click', () => {
+    navigateTo('/');
+  });
+  section.append(inputName, inputEmail, inputPass, buttonRegistro, buttonReturn);
+  return section;
+}
+
+export default registro;
