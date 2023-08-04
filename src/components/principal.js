5876ab6d50e977c10ad9358dc31dc6324b987a67
oolidu <arquitecturaod@gmail.com>
2023-08-03 22:24:44 -0400

    Sin muchos cambios

--- src/components/principal.js
+++ src/components/principal.js
@@ -1,9 +1,10 @@
-function principal(navigateTo) {
-const section = document.createElement('section')
-const prueba = document.createElement('h2');
-prueba.textContent=('Hola');
-section.appendChild(prueba)
-return section;
+// eslint-disable-next-line no-unused-vars
+function principal(_navigateTo) {
+  const section = document.createElement('section');
+  const prueba = document.createElement('h2');
+  prueba.textContent = ('Hola');
+  section.appendChild(prueba);
+  return section;
 }
 
 export default principal;
